9f0dcfbd935fff1530c1610a98c68d66cc97f9f6
Andy Brown <andybrown41@icloud.com>
2021-01-23 07:24:23 +0000

    Initial Commit

---
+++ PlaylistPlayer/PlaylistPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistPlayerApp.swift
+//  PlaylistPlayer
+//
+//  Created by Andy Brown on 23/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
